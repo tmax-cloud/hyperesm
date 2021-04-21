887ecb0a145a2ca4ed26a6afda76a70f5b2321b2
이승진 <seungjin_lee2@tmax.co.kr>
2021-04-21 13:50:14 +0900

    [mod] update Dockerfile, start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export K8S_HOME=/home/tmax/service-broker
-/usr/bin/java -jar -Dlogback.configurationFile=${K8S_HOME}/logback.xml ${K8S_HOME}/lib/template-service-broker.jar
+export K8S_HOME=/home/tmax/hyperesm
+/usr/bin/java -jar -Dlogback.configurationFile=${K8S_HOME}/logback.xml ${K8S_HOME}/lib/hyperesm.jar
